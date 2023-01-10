c0a05f554e49e6e86fcdd17a6865a008dc4c3450
Oleksii Adakh <lamealex641@gmail.com>
2023-01-10 18:06:16 +0200

    Add files via upload

---
+++ src/main/java/com/defatov/todolist_spring_usage/TodolistSpringUsageApplication.java
@@ -0,0 +1,13 @@
+package com.defatov.todolist_spring_usage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistSpringUsageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistSpringUsageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/defatov/todolist_spring_usage/TodolistSpringUsageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.defatov.todolist_spring_usage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistSpringUsageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
